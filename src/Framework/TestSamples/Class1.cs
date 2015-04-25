efad810c63a9d80a7e1c539a42807cc260a7f409
Tala <Karen.otte@motognosis.com>
2015-04-25 14:16:34 +0200

    Added NUnit-Library

--- src/Framework/TestSamples/Class1.cs
+++ src/Framework/TestSamples/Class1.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace TestSamples
 {
     public class Class1
     {
+        [Test]
+        public static void TestCase()
+        {
+            
+        }
+
     }
 }
