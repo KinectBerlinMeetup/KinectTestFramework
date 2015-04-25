26b111b44df321012cc9e7cc1b34c95c7ba1b458
Tala <Karen.otte@motognosis.com>
2015-04-25 15:56:59 +0200

    Play-methods added for Playback of Sequences

--- src/Framework/Framework/KinectTesting.cs
+++ src/Framework/Framework/KinectTesting.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Threading;
 using Microsoft.Kinect.Tools;
 
@@ -9,6 +10,9 @@ namespace Framework
         public static KStudioClient Client;
         public static KStudioPlayback Playback;
         public static ReadOnlyObservableCollection<KStudioEventStream> CurrentEventStreams;
+        public static EventHandler PlaybackFinished;
+
+        #region SetUp
 
         public static void SetupPlayback(string filepath)
         {
@@ -17,7 +21,6 @@ namespace Framework
             {
                 Playback = Client.CreatePlayback(filepath);
                 CurrentEventStreams = Playback.Source.EventStreams;
-                
             }
         }
 
@@ -44,6 +47,10 @@ namespace Framework
                 Client = KStudio.CreateClient();
             }
         }
+
+        #endregion
+
+        #region Extermination
 
         public static void ExterminatePlayback()
         {
@@ -73,6 +80,10 @@ namespace Framework
             }
         }
 
+        #endregion
+
+        #region Playback
+
         public static void PlaybackEverythingAndWait()
         {
             if (Playback != null)
@@ -80,7 +91,70 @@ namespace Framework
                 Playback.EndBehavior = KStudioPlaybackEndBehavior.Stop;
                 Playback.Start();
                 Thread.Sleep(Playback.Duration);
+                InvokePlaybackFinishedEvent();
             }
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="timing">Play with 30Hz or faster?</param>
+        /// <param name="start">Play from here</param>
+        /// <param name="end">Play till here</param>
+        public static void Play(PlaybackTiming timing = PlaybackTiming.Normal, TimeSpan? start = null, TimeSpan? end = null)
+        {
+            if (Playback == null) return;
+
+            switch (timing)
+            {
+                    case PlaybackTiming.Fast: 
+                        Playback.Mode = KStudioPlaybackMode.TimingDisabled;
+                        break;
+                    case PlaybackTiming.Normal: 
+                        Playback.Mode = KStudioPlaybackMode.TimingEnabled;
+                        break;
+            }
+
+            if (start != null)
+            {
+                Playback.InPointByRelativeTime = start.Value;
+            }
+
+            if (end != null && end < Playback.Duration)
+            {
+                Playback.OutPointByRelativeTime = end.Value;
+            }
+
+            Playback.EndBehavior = KStudioPlaybackEndBehavior.Stop;
+            Playback.Start();
+            Thread.Sleep(Playback.Duration);
+            InvokePlaybackFinishedEvent();
+        }
+
+        private static void InvokePlaybackFinishedEvent()
+        {
+            if (PlaybackFinished != null)
+            {
+                PlaybackFinished.Invoke(Playback, new EventArgs());
+            }
+        }
+
+        #endregion
+    }
+
+    public enum PlaybackTiming
+    {
+        Fast,
+        Normal
+    }
+
+    public enum EventStreams
+    {
+        All = 0,
+        Body = 1,
+        Depth = 2,
+        Ir = 4,
+        Color = 8,
+        Audio = 16
     }
 }
