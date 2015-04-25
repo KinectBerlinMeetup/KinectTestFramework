614d4ce2e7ce6091f3be388e9fd67d268aefdc18
Tala <Karen.otte@motognosis.com>
2015-04-25 14:10:52 +0200

    Added first Functions to Framework for Test-Setup and -Teardown

---
+++ src/Framework/Framework/KinectTesting.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Kinect.Tools;
+
+namespace Framework
+{
+    public static class KinectTesting
+    {
+        public static KStudioClient Client;
+        public static KStudioPlayback Playback;
+        public static ReadOnlyObservableCollection<KStudioEventStream> CurrentEventStreams;
+
+        public static void SetupPlayback(string filepath)
+        {
+            SetupClientAndConnect();
+            if (Client != null && Client.IsServiceConnected)
+            {
+                var playbackFile = Client.CreateEventFile(filepath);
+                CurrentEventStreams = playbackFile.EventStreams;
+                Playback = Client.CreatePlayback(playbackFile);
+            }
+        }
+
+        public static void SetupClientAndConnect()
+        {
+            SetupClient();
+            ConnectClientToService();
+        }
+
+        public static bool ConnectClientToService()
+        {
+            if (Client != null)
+            {
+                Client.ConnectToService();
+                return Client.IsServiceConnected;
+            }
+            return false;
+        }
+
+        public static void SetupClient()
+        {
+            if (Client == null)
+            {
+                Client = KStudio.CreateClient();
+            }
+        }
+
+        public static void ExterminatePlayback()
+        {
+            if (Playback != null)
+            {
+                CurrentEventStreams = null;
+                Playback.Dispose();
+            }
+        }
+
+        public static void ExterminateClient()
+        {
+            if (Client != null)
+            {
+                DisconnectClient();
+                Client.Dispose();
+            }
+        }
+
+        public static void DisconnectClient()
+        {
+            if (Client != null && Client.IsServiceConnected)
+            {
+                Client.DisconnectFromService();
+            }
+        }
+    }
+}
